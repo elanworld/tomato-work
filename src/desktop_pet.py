aab41e833917581defa413d9b8215ad304bc1cee
alan <elanworld@163.com>
2022-08-25 18:43:44 +0800

    api 超时设置

--- src/desktop_pet.py
+++ src/desktop_pet.py
@@ -21,15 +21,15 @@ class RelaxPet:
     def move(self, x, y, x1=None, y1=None, duration=None):
         body = {"x": x, "y": y, "x1": x1, "y1": y1, "duration": duration}
         if x1 is not None:
-            response = requests.post(self.app_api + "swap", json=body)
+            response = requests.post(self.app_api + "swap", json=body, timeout=5)
         else:
-            response = requests.post(self.app_api + "move", json=body)
+            response = requests.post(self.app_api + "move", json=body, timeout=5)
         if not response.text.startswith("{"):
             print(response.text)
 
     def state(self, transparency: float = 1):
         body = {"transparency": transparency}
-        response = requests.post(self.app_api + "state", json=body)
+        response = requests.post(self.app_api + "state", json=body, timeout=5)
         if not response.text.startswith("{"):
             print(response.text)
 
